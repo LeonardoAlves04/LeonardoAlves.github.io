b33601c63fe8f519cf3ea54638428473f5dcc253
Leonardo Alves <leonardoalves201510@gmail.com>
2023-02-08 14:20:44 -0300

    feat: bulding and styling navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetailsPage/itemDetails";
 
@@ -14,16 +16,17 @@ const ScrollToTop = () => {
   return null;
 }
 
-
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="item/:itemId" element={<ItemDetails />} />
           <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/succes" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div >
