e796f6a29819fc2baf9577622a94d9e68755eda7
Leonardo Alves <leonardoalves201510@gmail.com>
2023-02-08 13:35:11 -0300

    feat: starting to structure our project

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Checkout from "./scenes/checkout/Checkout";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetailsPage/itemDetails";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
 
 function App() {
   return (
     <div className="app">
-
-    </div>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
   );
 }
 
