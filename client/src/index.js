80e06377761eecb6baa16d8ee1b56ccc4d9d74bc
Leonardo Alves <leonardoalves201510@gmail.com>
2023-02-10 15:17:25 -0300

    .

--- client/src/index.js
+++ client/src/index.js
@@ -16,10 +16,12 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode >
 );
 
