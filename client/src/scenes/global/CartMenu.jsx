9ea803d57ff9462843b3431078f0582adb918107
Leonardo Alves <leonardoalves201510@gmail.com>
2023-02-14 21:52:30 -0300

    feat: adding a cart menu

---
+++ client/src/scenes/global/CartMenu.jsx
@@ -0,0 +1,72 @@
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Remove";
+import styled from "@emotion/styled";
+import { shades } from "../../theme";
+import {
+  decreaseCount,
+  increaseCount,
+  removeFromCart,
+  setIsCartOpen,
+} from "../../state";
+import { useNavigate } from "react-router-dom";
+
+const FlexBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CartMenu = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.attributes.price;
+  }, 0);
+
+  return (
+    <Box //sobreposição do modal
+      display={isCartOpen ? "block" : "none"}
+      backgroundColor="rgba(0,0,0,0.4)"
+      position="fixed"
+      zIndex={10}
+      width="100%"
+      height="100%"
+      left="0"
+      top="0"
+      overflow="auto"
+    >
+      <Box //modal
+        position="fixed"
+        right="0"
+        bottom="0"
+        width="max(400px,30%)"
+        height="100%"
+        backgroundColor="white"
+      >
+        <Box padding="30px" overflow="auto" height="100%">
+          {/*cabeçalho do modal*/}
+          <FlexBox mb="15px">
+            <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+            <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+              <CloseIcon />
+            </IconButton>
+          </FlexBox>
+
+          {/* lista do carrinho */}
+          <Box>
+            {cart.map((item)=> (
+                <Box>
+            ))}
+          </Box>
+
+
+        </Box>
+      </Box>
+    </Box>
+  );
+};
