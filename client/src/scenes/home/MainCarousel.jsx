8f8e62dfbdd720d67b97ec0d203ebaaa78f093da
Leonardo Alves <leonardoalves201510@gmail.com>
2023-02-22 10:59:45 -0300

    ending  MainCarousel building

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -5,6 +5,75 @@ import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
 
-const MainCarousel = () => {};
+// importando todas as imagens de uma vez
+
+const importAll = (r) =>
+  r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
+
+const heroTextureImports = importAll(
+  require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+);
+
+const MainCarousel = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  return (
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+    >
+      {Object.values(heroTextureImports).map((texture, index) => (
+        <Box key={`carousel-image-${index}`}>
+          <img
+            src={texture}
+            alt={`carousel-${index}`}
+            style={{
+              width: "100%",
+              height: "700px",
+              objectFit: "cover",
+              backgroundAttachment: "fixed",
+            }}
+          />
+          <Box color="white"></Box>
+        </Box>
+      ))}
+    </Carousel>
+  );
+};
 
 export default MainCarousel;
