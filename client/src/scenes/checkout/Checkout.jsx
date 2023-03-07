c04c36e136760c959682612f0645463dbaecfcec
Leonardo Alves <leonardoalves201510@gmail.com>
2023-03-06 23:20:06 -0300

    feat: building checkout page

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,5 +1,116 @@
+import { useSelector } from "react-redux";
+import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
+import { Formik } from "formik";
+import { useState } from "react";
+import * as yup from "yup";
+import { shades } from "../../theme";
+
+
+const initialValues = { 
+billingAdress : {
+ firstName: "",
+ lastName: "",
+ country: "",
+ street1:"",
+ street2:"",
+ city:"",
+ state:"",
+ zipCode:"",
+},
+shippingAdress:{
+    isSameAddress: true,
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1:"",
+    street2:"",
+    city:"",
+    state:"",
+    zipCode:"",
+},
+email:"",
+phoneNumber:""
+}
+
+const checkoutSchema = [
+    yup.object().shape({
+        billingAdress : yup.object().shape({
+            firstName: yup.string().required("required"),
+            lastName: yup.string().required("required"),
+            country: yup.string().required("required"),
+            street1:yup.string().required("required"),
+            street2:yup.string(),
+            city:yup.string().required("required"),
+            state:yup.string().required("required"),
+            zipCode:yup.string().required("required"), 
+        }),
+        shippingAdress : yup.object().shape({
+            isSameAddress: yup.boolean(),
+            firstName: yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            lastName: yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            country: yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            street1:yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            street2:yup.string(),
+            city:yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            state:yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            zipCode:yup.string().when("isSameAdress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+        }),
+
+    })
+]
+
 const Checkout = () => {
-    return <div>Checkout</div>
+  const [activeStep, setActive] = useState(0);
+  const cart = useSelector((state) => state.cart.cart);
+  const isFirstStep = activeStep === 0;
+  const isSecondStep = activeStep === 1;
+
+  const handleFormSubmit = async (value, actions) => {
+    setActive(activeStep + 1);
+  };
+
+  async function makePayment(values) {}
+
+  return (
+    <Box width="80%" m="100px auto">
+      <Stepper activeStep={activeStep} sx={{ m: "20px 0" }}>
+        <Step>
+          <StepLabel>Cobrança</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pagamento</StepLabel>
+        </Step>
+      </Stepper>
+      <Box>
+        <Formik onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={}>
+
+        </Formik>
+      </Box>
+    </Box>
+  );
 };
 
 export default Checkout;
