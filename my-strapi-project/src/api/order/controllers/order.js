0ef557471787ed6668c5b5944970f6f1566dc426
Leonardo Alves <leonardoalves201510@gmail.com>
2023-03-22 15:32:41 -0300

    feat: set the secret key for stripe payment

--- my-strapi-project/src/api/order/controllers/order.js
+++ my-strapi-project/src/api/order/controllers/order.js
@@ -1,9 +1,21 @@
 'use strict';
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 /**
  * order controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+    async create(ctx) {
+        const { products, userName, email } = ctx.request.body;
+
+        try {
+            const lineItems = await Promise.all(
+                products.map(async (product) => {
+                    const item = await strapi.service("api::item.item")
+                })
+            )
+        } catch (error) { }
+    }
+}));
