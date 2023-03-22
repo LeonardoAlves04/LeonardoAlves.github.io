026af1fe22368e034c3c44862ddc40ee6766d5dc
Leonardo Alves <leonardoalves201510@gmail.com>
2023-03-22 16:11:50 -0300

    feat: finish the building of stripe backend

--- my-strapi-project/src/api/order/controllers/order.js
+++ my-strapi-project/src/api/order/controllers/order.js
@@ -13,9 +13,40 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
         try {
             const lineItems = await Promise.all(
                 products.map(async (product) => {
-                    const item = await strapi.service("api::item.item")
+                    const item = await strapi
+                        .service("api::item.item")
+                        .findOne(product.id);
+
+                    return {
+                        price_data: {
+                            currency: "usd",
+                            product_data: {
+                                name: item.name
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        quantity: product.count
+                    }
                 })
-            )
-        } catch (error) { }
-    }
+            );
+
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: [card],
+                costumer_email: email,
+                mode: "payment",
+                success_url: "https://localhost:3000/checkout/success",
+                cancel_url: "http://localhost:3000",
+                line_items: lineItems
+            });
+
+            await strapi.service("api::order.order").create({
+                data: { userName, products, stripeSessionId: session.id },
+            });
+
+            return { id: session.id };
+        } catch (error) {
+            ctx.response.status = 500;
+            return { error: { message: "There was a problem creating the charge." } };
+        }
+    },
 }));
