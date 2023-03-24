81524e31886fdb91e480c2e3cc1a8d6d16c80f24
Leonardo Alves <leonardoalves201510@gmail.com>
2023-03-24 15:02:30 -0300

    feat: building the confirmation message

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,14 +1,12 @@
-import { Box } from "@mui/material";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
+import { Box, Alert, AlertTitle } from "@mui/material";
 
 const Confirmation = () => {
   return (
     <Box m="90px auto" width="80%" height="50vh">
       <Alert severity="success">
-        <AlertTitle>Success</AlertTitle>
-        You have successfully made an Order —{" "}
-        <strong>Congrats on Making your Purchase</strong>
+        <AlertTitle>Confirmado!</AlertTitle>
+        Você acaba de confirmar o seu pedido —{" "}
+        <strong>Parabéns pela sua compra e obrigado!</strong>
       </Alert>
     </Box>
   );
