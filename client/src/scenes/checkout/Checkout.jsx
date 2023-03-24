db54d7cedb67bb65559ec5bdb8f5f86ed5a8619f
Leonardo Alves <leonardoalves201510@gmail.com>
2023-03-24 15:32:56 -0300

    fix: fixing payment form error

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -4,12 +4,12 @@ import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
 import { shades } from "../../theme";
-import Shipping from "./Shipping";
 import Payment from "./Payment";
+import Shipping from "./Shipping";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-  "pk_test_51Mo9IdEvGHJfN2RqUbDgWE5PnHMVxV1mDTfYHJCRTQmjxkUfgyDBwd6i4hd2uDhQieyDleTcypfXLWNxSJ2ZHGls00x79NrfDt"
+  "pk_test_51LgU7yConHioZHhlAcZdfDAnV9643a7N1CMpxlKtzI1AUWLsRyrord79GYzZQ6m8RzVnVQaHsgbvN1qSpiDegoPi006QkO0Mlc"
 );
 
 const Checkout = () => {
@@ -21,6 +21,7 @@ const Checkout = () => {
   const handleFormSubmit = async (values, actions) => {
     setActiveStep(activeStep + 1);
 
+    // this copies the billing address onto shipping address
     if (isFirstStep && values.shippingAddress.isSameAddress) {
       actions.setFieldValue("shippingAddress", {
         ...values.billingAddress,
@@ -61,10 +62,10 @@ const Checkout = () => {
     <Box width="80%" m="100px auto">
       <Stepper activeStep={activeStep} sx={{ m: "20px 0" }}>
         <Step>
-          <StepLabel>Cobrança</StepLabel>
+          <StepLabel>Billing</StepLabel>
         </Step>
         <Step>
-          <StepLabel>Pagamento</StepLabel>
+          <StepLabel>Payment</StepLabel>
         </Step>
       </Stepper>
       <Box>
@@ -104,7 +105,7 @@ const Checkout = () => {
                 />
               )}
               <Box display="flex" justifyContent="space-between" gap="50px">
-                {isSecondStep && (
+                {!isFirstStep && (
                   <Button
                     fullWidth
                     color="primary"
@@ -118,7 +119,7 @@ const Checkout = () => {
                     }}
                     onClick={() => setActiveStep(activeStep - 1)}
                   >
-                    Voltar
+                    Back
                   </Button>
                 )}
                 <Button
@@ -134,7 +135,7 @@ const Checkout = () => {
                     padding: "15px 40px",
                   }}
                 >
-                  {isFirstStep ? "Próximo" : "Fazer Pedido"}
+                  {!isSecondStep ? "Next" : "Place Order"}
                 </Button>
               </Box>
             </form>
